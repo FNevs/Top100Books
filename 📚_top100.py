8cea70d10a52b29c13b8b6c76eab5f3247c7091f
FNevs <filipesn.inc@gmail.com>
2024-12-23 15:47:40 -0300

    Minor adjustments across the code

--- 📚_top100.py
+++ 📚_top100.py
@@ -1,3 +1,5 @@
+# https://www.kaggle.com/datasets/anshtanwar/top-200-trending-books-with-reviews
+
 import streamlit as st  # Library for creating web apps
 import pandas as pd # Library for data manipulation
 import plotly.express as px  # Library for creating interactive plots
@@ -5,8 +7,8 @@ import plotly.express as px  # Library for creating interactive plots
 st.set_page_config(layout="wide")	# Wide layout
 
 # Load the dataset
-df_reviews = pd.read_csv('datasets/customer reviews.csv') 
-df_top100_books = pd.read_csv('datasets/Top-100 Trending Books.csv')    
+df_reviews = pd.read_csv('dataset/customer reviews.csv') 
+df_top100_books = pd.read_csv('dataset/Top-100 Trending Books.csv')    
 
 df_reviews
 
