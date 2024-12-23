9472e304d7be23c432e1fad1eb5ee74804d5f194
FNevs <filipesn.inc@gmail.com>
2024-12-23 12:53:49 -0300

    Setup Streamlit and load initial data

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+import pandas as pd
+
+st.set_page_config(layout="wide")	# Wide layout
+
+df_reviews = pd.read_csv('datasets/customer reviews.csv')
+df_top100_books = pd.read_csv('datasets/Top-100 Trending Books.csv')
+
+df_reviews
